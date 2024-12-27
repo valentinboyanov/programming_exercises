10a6667dbc1e6c925eb68dd35d4994b5af68899e
Valentin Rusev <val.rusev@gmail.com>
2024-12-27 21:16:15 +0100

    From unittest to doctest

--- simple_programming_problems/solutions/lists_strings_05.py
+++ simple_programming_problems/solutions/lists_strings_05.py
@@ -1,13 +1,11 @@
-import unittest
-from typing import List
-
 # 5. Write a function that computes the running total of a list.
+
+from typing import List
 
 
 def running_total(elements: List[int]) -> int:
+    """
+    >>> running_total([2, 3, 4])
+    9
+    """
     return sum(elements)
-
-
-class RunningTotalTest(unittest.TestCase):
-    def test_running_total(self):
-        self.assertEqual(9, running_total([2, 3, 4]))
